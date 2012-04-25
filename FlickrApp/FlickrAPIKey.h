cebaf896b9fd6705fef38c45e97bb83347f138c3
Drew Rosenberg <drewrosenberg@gmail.com>
2012-04-24 23:38:40 -0700

    blank app with flickr fetcher and my flickr key added

---
+++ FlickrApp/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"d7d39db8fadb119221b4998eb1481156"
