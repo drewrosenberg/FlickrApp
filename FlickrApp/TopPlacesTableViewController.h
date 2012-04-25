b7ed701d40333288c6715a7b776bb3d7e61bc269
Drew Rosenberg <drewrosenberg@gmail.com>
2012-04-24 23:46:06 -0700

    Added 2 table view controllers and roughed in iPhone storyboard

---
+++ FlickrApp/TopPlacesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesTableViewController.h
+//  FlickrApp
+//
+//  Created by Drew Rosenberg on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesTableViewController : UITableViewController
+
+@end
